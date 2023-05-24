29ca9cfb2f376d3222e6125995bad1aa019c1604
dennis-tech <hedenniskibe@gmail.com>
2023-05-24 05:09:30 +0300

    refactored the css and js codes

--- index.js
+++ index.js
@@ -4,22 +4,30 @@ import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebas
 const appSettings = {
     databaseURL: "https://realtime-database-46bf3-default-rtdb.firebaseio.com/"
 }
-
+//database
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const endorseListInDB = ref(database, 'endorseList');
 
+//from html
 const endorseInputEl = document.getElementById("endorseInputField");
+const inputFromEl = document.getElementById("inputFrom");
+const inputToEl = document.getElementById("inputTo");
 const publishBtnEl = document.getElementById("publishBtn");
 const endorseListEl = document.getElementById("endorseList");
 
+//publish btn function
 publishBtnEl.addEventListener("click", function() {
-    let endorseInput = endorseInputEl.value;
+    const endorseInput = endorseInputEl.value;
+    const inputFrom = inputFromEl.value;
+    const inputTo = inputToEl.value;
+    const inputArray = [endorseInput, inputFrom, inputTo]
     if (endorseInput) {
-        push(endorseListInDB, endorseInput);
+        push(endorseListInDB, inputArray);
         clearEndorseInputEl();
     }
 })
+// displaying the endorsements
 onValue(endorseListInDB, (snapshot) => {
     if(snapshot.exists()){
         let endorseList = Object.entries(snapshot.val());
@@ -31,22 +39,39 @@ onValue(endorseListInDB, (snapshot) => {
             appendEndorseToDOM(currentEndorse);
         }
     } else {
-        endorseListEl.innerHTML = '<span>No items here... yet</span>';
+        endorseListEl.innerHTML = '<span class="noEndorse">No Endorsements here... yet</span>';
 
     }
 });
+
+// clear endorse list function
 function clearEndorseListEl(){
     endorseListEl.innerHTML = '';
 }
-function appendEndorseToDOM(endorse){
-    let endorseID = endorse[0];
-    let endorseValue = endorse[1];
 
+// appending the endorsements to the DOM
+function appendEndorseToDOM(endorse){
+    //let endorseID = endorse[0];
+    let endorseValueEndorse = endorse[1][0];
+    let endorseValueFrom = endorse[1][1];
+    let endorseValueTo = endorse[1][2];
     let endorseEl = document.createElement('li');
-    endorseEl.textContent = endorseValue;
+    
+    endorseEl.innerHTML = `
+    <span class="endorseValueTo">To ${endorseValueTo}<br></span>
+    <span class="endorseValueEndorse">${endorseValueEndorse}<br></span>
+    <span class="endorseValueFrom">From ${endorseValueFrom}</span>
+    <i class="fa fa-heart" id="fa-heart"></i>
+    <p id="likesCount"></p>
+    `;
     endorseListEl.append(endorseEl);
 }
+// clear input function
 function clearEndorseInputEl() {
-    endorseInputEl.value = '';
+    endorseInputEl.value = "";
+    inputFromEl.value = "";
+    inputToEl.value = "";
 }
+
+
 
